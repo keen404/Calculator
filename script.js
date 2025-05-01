9738e7cee561b993063a52e4811a3dcaff28b685
Keen <2003keen@gmail.com>
2025-05-01 15:10:31 +0700

    Create common function for calculator

---
+++ script.js
@@ -0,0 +1,19 @@
+const add = function(a, b) {
+    return +a + +b;;
+  };
+
+const subtract = function(a, b) {
+    return +a - +b;
+};
+
+const multiply = function(arr) {
+    return arr.reduce((accumulator, value) => accumulator * +value);
+};
+
+const divide = function(a, b) {
+    return +a / +b
+}
+
+let firstNum = 0;
+let secocndNum = 0;
+let operlator = '';
