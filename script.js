8a16cb3b541e82fb997c73df4705dc4477ac1bac
Keen <2003keen@gmail.com>
2025-05-06 08:34:08 +0700

    Implement operator button

--- script.js
+++ script.js
@@ -17,12 +17,22 @@ const divide = function(a, b) {
 
 const displayDiv = document.querySelector(".display");
 const digitBtns = document.querySelectorAll(".digit");
+
 digitBtns.forEach(function(btn) {
     btn.addEventListener("click", () => {
         displayDiv.textContent += btn.textContent;
     })
 })
 
+let firstNumber = 0;
+const operatorBtns = document.querySelectorAll(".operator");
+operatorBtns.forEach(function(btn) {
+    btn.addEventListener("click", () => {
+        firstNumber = displayDiv.textContent;
+        displayDiv.textContent = '';
+        console.log(firstNumber)
+    })
+})
 // When press operator button 
 // Hold Current button
 // Empty the displaying text
